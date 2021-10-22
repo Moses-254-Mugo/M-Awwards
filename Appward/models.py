9390d5baf9e0e61c728489a04463576d17cae7ac
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-10-22 22:52:55 +0300

    Add class model profile

--- Appward/models.py
+++ Appward/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    profile_pic = models.ImageField(upload='images/')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500)
+    desc = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.user
+    
+    def save_profile(self):
+        return self.save()
+
+    def delete_profile(self):
+        return self.delete()
+
