d784f47db42b74f1d2164517afc9f0ad390799fa
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-10-22 23:12:58 +0300

    Add model project

--- Appward/models.py
+++ Appward/models.py
@@ -6,7 +6,7 @@ class Profile(models.Model):
     profile_pic = models.ImageField(upload='images/')
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500)
-    desc = models.TextField(max_length=500)
+    information = models.TextField(max_length=500)
 
     def __str__(self):
         return self.user
@@ -16,4 +16,30 @@ class Profile(models.Model):
 
     def delete_profile(self):
         return self.delete()
+
+class Project(models.Model):
+    title = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='images/')
+    description = models.CharField(max_length=300)
+    link = models.URLField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def save_project(self):
+        return self.save()
+    
+    @classmethod
+    def all_projects(cls):
+        all_projects = cls.objects.all()
+        return all_projects
+
+    @classmethod
+    def single_project(cls, id):
+        single_project= cls.objects.filter(id=id)
+        return single_project
+
+    @classmethod
+    def search_project(cls, search_term):
+        search_project = cls.objects.filter(title=search_term)
+        return search_project
 
