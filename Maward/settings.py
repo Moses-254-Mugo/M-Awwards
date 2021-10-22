63611d870f289e15b9e5e5180690110183f3db07
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-10-22 16:12:51 +0300

    Create navbar

--- Maward/settings.py
+++ Maward/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Appward'
+    'Appward',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
