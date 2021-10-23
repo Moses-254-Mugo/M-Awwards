e1632329290e6365ef5c9b0fdc6360db457957c9
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-10-23 21:44:38 +0300

    Add route view single project

--- Appward/views.py
+++ Appward/views.py
@@ -58,7 +58,31 @@ def new_project(request):
 
 @login_required(login_url='/accounts/login/')
 def single_project(request, id):
-    
+    project = Project.objects.get(id=id)
+    comments = Comments.objects.filter(project_id=id)
+    rates = Rating.objects.filter(project_id = id)
+    designrate = []
+    usabilityrate =[]
+    contentrate = []
+
+    if rates:
+        for rates in rates:
+            designrate.append(rate.design)
+            usabilityrate.append(rate.usability)
+            contentrate.append(rate.content)
+        
+        total = len(designrate)*10
+        design = round(sum(designrate)/total*100, 1)
+        usability = round(sum(usabilityrate)/total*100,1)
+        content = round(sum(contentrate)/total*100,1)
+        return render(request, 'single_project.html',{'project':project, 'comments':comments,'design': design,'usability': usability, 'content':content})
+
+    else:
+        design = 0
+        usability = 0
+        content = 0
+
+        return render(request, 'single_project.html',{'project':project, 'comments':comments, 'design': design,'usability':usability, 'content':content})
 
 
 
