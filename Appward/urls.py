5111ee2f847b338c5f22ef99f5bf744aab5153db
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-10-23 18:11:47 +0300

    Add ulr for index

--- Appward/urls.py
+++ Appward/urls.py
@@ -5,7 +5,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', views.welcome, name='welcome'),
+    # url(r'^$', views.welcome, name='welcome'),
+    url(r'^$',views.index,name='home'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
